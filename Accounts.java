21b167a13fe53585dcaa5de182bc672a35bb232d
Swaminathan V <sv0829@srmist.edu.in>
2026-08-29 09:04:09 +0530

    final commit 1

--- Accounts.java
+++ Accounts.java
@@ -1,14 +1,15 @@
 public class Accounts{
-    int accountNumber,age ;
-    double balance;
-    String name,accounttype,status;
+    private int accountNumber,age ;
+    private double balance;
+    private String name,accountType,status;
 
-    public Accounts() {
-        accountNumber = 1001;
-        balance = 500;
-        age = 21;
-        name = "Abcd";
-        accounttype = "saving";
+    public Accounts(int accountNumber, String name, int age, double initialBalance, String accountType
+                    ) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+        this.age = age;
+        this.name = name;
+        this.accountType = accountType;
         status = "Active";
         System.out.println("Account has been creted");
 
@@ -19,12 +20,54 @@ public class Accounts{
             return false;
         }
         this.balance += amount;
+        System.out.println("Balance : " + this.balance);
         return true;
 
     }
+    boolean withdraw(double amount){
+        if (amount > this.balance){
+            return false;
+        }
+        this.balance -= amount;
+        System.out.println("Balance : " + this.balance);
+        return true;
+    }
+
+    int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    String getName(){
+        return this.name;
+    }
+
+    int getAge(){
+        return this.age;
+    }
+
+    double getBalance(){
+        return this.balance;
+    }
+
+    String getAccountType(){
+        return this.accountType;
+    }
+
+    String getStatus(){
+        return this.status;
+    }
+
+    void setName(String name){
+        this.name = name;
+        return;
+    }
+
+    void setAge(int age){
+        this.age = age;
+        return;
+    }
 
     public static void main(String[] args) {
-        Accounts a1 = new Accounts();
     }
 
 }
